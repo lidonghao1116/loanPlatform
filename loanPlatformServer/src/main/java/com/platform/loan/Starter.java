1b54042f6c6cf5c5698422eb4d5eecf8c4e10878
wyp12 <caogu.wyp@alibaba-inc.com>
2018-05-03 23:49:28 +0800

    wyp12
    init

--- loanPlatformServer/src/main/java/com/platform/loan/Starter.java
+++ loanPlatformServer/src/main/java/com/platform/loan/Starter.java
@@ -1,4 +1,4 @@
-package com.platform.loan.controller;
+package com.platform.loan;
 
 /**
 * Alipay.com Inc. Copyright (c) 2004-2018 All Rights Reserved.
@@ -13,11 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version $Id: Main.java, v 0.1 2018-03-08 下午5:14 caogu.wyp Exp $$
  */
 @SpringBootApplication
-public class Main {
+public class Starter {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(Starter.class, args);
 
     }
 }
---
+++ src/main/java/com/platform/loan/Starter.java
@@ -0,0 +1,23 @@
+package com.platform.loan;
+
+/**
+* Alipay.com Inc. Copyright (c) 2004-2018 All Rights Reserved.
+*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author caogu.wyp
+ * @version $Id: Main.java, v 0.1 2018-03-08 下午5:14 caogu.wyp Exp $$
+ */
+@SpringBootApplication
+public class Start {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Start.class, args);
+
+    }
+}
